6968b6310011a67ccefbdffedcdecde93e048f8b
Pratham Bansal <bansalpratham.bt21cse@pec.edu.in>
2023-03-04 16:14:39 +0530

    Update E_Negatives_and_Positives.cpp

--- PCD_CF849_Div4/E_Negatives_and_Positives.cpp
+++ PCD_CF849_Div4/E_Negatives_and_Positives.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
 
+void solve(){
+    int n;
+    cin>>n;
+    
+    int absum=0;
+    int count=0;
+    int min1=INT_MAX;
+    
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        if(x<0){
+            count++;
+        }
+        absum+=abs(x);
+        min1=min(min1,abs(x));
+    }
+
+    if(count&1){
+        cout<<absum-(2*min1)<<endl;
+    }
+    else{
+        cout<<absum<<endl;
+    }
+
+}
+signed main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
